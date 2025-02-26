4db8680661a87891e5a1d1c53d2baa9f8a2bc3d0
Leonardo Peres <leo_ps.12@hotmail.com>
2025-02-26 14:20:44 -0300

    Complete deployment and DevOps setup with CI/CD, Sentry, and Vercel Analytics

--- next.config.js
+++ next.config.js
@@ -1,6 +1,29 @@
 /** @type {import('next').NextConfig} */
+const { withSentryConfig } = require("@sentry/nextjs");
+
 const nextConfig = {
   /* config options here */
 };
 
-module.exports = nextConfig;
+// For all available options, see:
+// https://github.com/getsentry/sentry-webpack-plugin#options
+
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  org: process.env.SENTRY_ORG,
+  project: process.env.SENTRY_PROJECT,
+
+  // An auth token is required for uploading source maps to Sentry.
+  // You can get an auth token from https://sentry.io/settings/account/api/auth-tokens/
+  // The token must have `project:releases` and `org:read` scopes for source map upload.
+  authToken: process.env.SENTRY_AUTH_TOKEN,
+
+  silent: true, // Suppresses all logs
+};
+
+module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
